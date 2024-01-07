b5637af18e39bfa11abc7e0d7d2ba4e08f1fa782
swedenebomi <swedenebomi@gamil.com>
2024-01-07 17:34:32 +0100

    going on array list n map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+import './App.css';
+
+const finalSpaceCharacters = [
+  {
+    id:'gray',
+    name: 'Gary Goodspeed',
+    thumb: '/images/gary.png'
+  },
+  {
+    id:'cato',
+    name: 'Little Cato',
+    thumb: '/images/cato.png'
+  },
+  {
+    id:'',
+    name:'',
+    thumb:'/',
+  },
+  {
+    id:'',
+    name:'',
+    thumb:'/',
+  },
+  {
+    id:'',
+    name:'',
+    thumb:'/',
+  },
+  {
+    id:'',
+    name:'',
+    thumb:'/',
+  }
+
+]
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <header className="App-header">
+        <h1> Final Space Characters</h1>
+
+        <ul className='characters'>
+          {finalSpaceCharacters.map(({id,name,thumb})=> {
+            return (
+              <li key={id}>
+                <div className='characters-thumb'>
+                  <img />
+                </div>
+              </li>
+            )
+          })}
+
+        </ul>
+      </header>
+      
+    </div>
   )
 }
 
-export default App
+export default App;
